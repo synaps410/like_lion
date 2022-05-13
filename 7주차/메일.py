8c5448665e5bebfb2d01fa37c2056898aa92c3a4
synaps410 <101447444+synaps410@users.noreply.github.com>
2022-05-13 22:55:48 +0900

    Add files via upload

---
+++ 7주차/메일.py
@@ -0,0 +1,34 @@
+import smtplib
+from email.message import EmailMessage
+import imghdr
+import re
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 465
+
+def sendEmail(addr):
+    reg = "^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$"
+    if bool(re.match(reg,addr)):
+        smtp.send_message(message)
+        print("정상적으로 메일이 발송되었습니다.")
+    else:
+        print("유효한 이메일 주소가 아닙니다.")
+
+message = EmailMessage()
+message.set_content("코드라이언 복습중입니다.")
+
+message["Subject"] = "제목"
+message["From"] = "kingofbrian410@likelion.org"
+message["To"] = "synaps410@gmail.com"
+
+with open("왓챠.jpg","rb") as image:
+    image_file = image.read()
+
+image_type = imghdr.what('codelion',image_file)
+message.add_attachment(image_file,maintype='image',subtype=image_type)
+
+smtp = smtplib.SMTP_SSL(SMTP_SERVER,SMTP_PORT)
+smtp.login("kingofbrian410@likelion.org","85831343")
+
+sendEmail("kingofbrian410@likelion.org")
+smtp.quit()
