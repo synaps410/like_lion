ea03c1afb7d7945551d50846ed7aa7db45101cb8
synaps410 <101447444+synaps410@users.noreply.github.com>
2022-05-21 01:22:27 +0900

    Add files via upload
    8주차 (java script-일단 완료한 부분까지 올립니다. 다 못했어요ㅠㅠ)

---
+++ 8주차/session2/practice.js
@@ -0,0 +1,5 @@
+// document.getElementById('lion').style.color='red'
+// document.getElementById('tiger').style.color='blue'
+// document.getElementById('bear').style.color='green'
+
+document.getElementsByTagName('li')[0].style.color='red';
