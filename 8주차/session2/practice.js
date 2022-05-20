3cbe46405ae70ce396711677e3758657957201b8
synaps410 <101447444+synaps410@users.noreply.github.com>
2022-05-21 01:22:56 +0900

    Delete 8주차 directory

--- 8주차/session2/practice.js
+++
@@ -1,5 +0,0 @@
-// document.getElementById('lion').style.color='red'
-// document.getElementById('tiger').style.color='blue'
-// document.getElementById('bear').style.color='green'
-
-document.getElementsByTagName('li')[0].style.color='red';
